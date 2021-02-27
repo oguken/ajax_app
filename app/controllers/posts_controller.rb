a4593e7b0e7888840997edc6955ae664703aa258
oguken <gura.time@icloud.com>
2021-02-27 19:12:36 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(id: "DESC")
   end
-end
+
+  def create
+    Post.create(content: params[:content])
+  end
+
+end  
