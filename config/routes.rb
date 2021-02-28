d92a15ba02ff31a31bca0ca80fe19bf68dffca07
oguken <gura.time@icloud.com>
2021-02-28 12:58:14 +0900

    Revert "既読機能のサーバーサイド実装"
    This reverts commit f0ffc59d6cac4d126884dc638ee56745e49a7b7e.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'posts#index' 
-  post 'posts', to: 'posts#create'
-  get 'posts/:id', to: 'posts#checked'
+  root to: 'posts#index' # ←編集
+    post 'posts', to: 'posts#create'
 end
